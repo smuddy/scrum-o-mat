be3321b199e8b3ddcfaba280d624aa85a2b55365
MothareenRahman <Mothareen.Rahman@on-geo.de>
2020-12-17 16:10:21 +0100

    [BAUFI-2809] Nutzer-entfernen-imroved01

--- src/app/modules/scrum-master/components/developers/developers.component.ts
+++ src/app/modules/scrum-master/components/developers/developers.component.ts
@@ -1,38 +1,37 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {PlanningService, renderStorypoint} from '../../../../services/planning.service';
-import {Developer} from '../../../../models/delevoper';
-import {Storypoints} from '../../../../models/storypoints';
-import {faTimes} from '@fortawesome/free-solid-svg-icons';
-import {faTrash} from '@fortawesome/free-solid-svg-icons';
-import {ActivatedRoute, Router} from '@angular/router';
-import {AdminService} from '../../../admin/components/admin.service';
-import {Observable} from 'rxjs';
-import {AngularFirestore, AngularFirestoreDocument} from '@angular/fire/firestore';
-import { debounce } from 'rxjs/operators';
+import {Component, Input, OnInit}from '@angular/core';
+import {PlanningService, renderStorypoint}from '../../../../services/planning.service';
+import {Developer}from '../../../../models/delevoper';
+import {Storypoints}from '../../../../models/storypoints';
+import {faTimes}from '@fortawesome/free-solid-svg-icons';
+import {faTrash}from '@fortawesome/free-solid-svg-icons';
+import {ActivatedRoute, Router}from '@angular/router';
+import {AdminService}from '../../../admin/components/admin.service';
+import {Observable}from 'rxjs';
+import {AngularFirestore, AngularFirestoreDocument}from '@angular/fire/firestore';
+import {debounce }from 'rxjs/operators';
 
-@Component({
-  selector: 'app-developers',
-  templateUrl: './developers.component.html',
-  styleUrls: ['./developers.component.less']
+@Component( {
+  selector:'app-developers',
+  templateUrl:'./developers.component.html',
+  styleUrls:['./developers.component.less']
 })
 export class DevelopersComponent implements OnInit {
-  @Input() public developers: { id: string; data: Developer }[];
-  @Input() public showResults: boolean;
+  @Input()public developers: {id:string; data:Developer }[];
+  @Input()public showResults:boolean;
   public faTimes = faTimes;
   public faTrash = faTrash;
-  public planningId: string;
-  public isPresent: boolean= false;
+  public planningId:string;
 
 
-  public users: { id: string, data: Developer }[];
+  public users: {id:string, data:Developer }[];
   // tslint:disable-next-line:max-line-length
 
   constructor(
-    private activatedRoute: ActivatedRoute,
-    private planningService: PlanningService,
-    private router: Router,
-    private adminService: AdminService,
-    private afs: AngularFirestore) {
+    private activatedRoute:ActivatedRoute,
+    private planningService:PlanningService,
+    private router:Router,
+    private adminService:AdminService,
+    private afs:AngularFirestore) {
     activatedRoute.params.subscribe(_ => this.planningId = _.planningId);
   }
   ngOnInit() {
@@ -42,19 +41,19 @@ export class DevelopersComponent implements OnInit {
     }
   }
 
-  public async delete(id: string) {
+  public async delete(id:string) {
     await this.adminService.deleteUser(this.planningId, id);
   }
 
-  renderStorypoints(storypoints: Storypoints): string {
+  renderStorypoints(storypoints:Storypoints):string {
     return renderStorypoint(storypoints);
 
   }
-  public devIsReady(storypoints: Storypoints) {
+  public devIsReady(storypoints:Storypoints) {
     const hasStorypoints = storypoints != null;
     const unsure = storypoints === Storypoints.unsure;
 
-    return (!this.showResults && hasStorypoints) || (this.showResults && !unsure);
+    return ( ! this.showResults && hasStorypoints) || (this.showResults &&  ! unsure);
   }
 
 
