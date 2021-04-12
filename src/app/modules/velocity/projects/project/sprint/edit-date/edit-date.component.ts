867967bbd7b9a6f0336c2ccde200d3b27363e1c1
benjamin <smuddyx@gmail.com>
2021-04-12 21:39:19 +0200

    added logo

--- src/app/modules/velocity/projects/project/sprint/edit-date/edit-date.component.ts
+++ src/app/modules/velocity/projects/project/sprint/edit-date/edit-date.component.ts
@@ -1,26 +1,18 @@
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
-import {Subscription} from 'rxjs';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-edit-date',
   templateUrl: './edit-date.component.html',
   styleUrls: ['./edit-date.component.less']
 })
-export class EditDateComponent implements OnInit, OnDestroy {
+export class EditDateComponent {
 
   public boundDate: Date;
   @Output() dateChanged = new EventEmitter<Date>();
-  private sub: Subscription;
 
   @Input() set date(date: Date) {
-    console.log(date);
     this.boundDate = date;
   }
-
-  ngOnInit = (): void => {
-  };
-
-  ngOnDestroy = (): void => this.sub.unsubscribe();
 
   onDateChange($event: any) {
     this.dateChanged.emit($event.target.valueAsDate);
